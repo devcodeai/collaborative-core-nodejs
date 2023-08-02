b55bb68dc150fc99688918ef782e709c9ebb03d1
Suryanto Tan <suryanto.tan5@gmail.com>
2023-08-02 10:44:25 +0700

    setup DB

---
+++ src/database/config.js
@@ -0,0 +1,109 @@
+import { createPool } from 'mysql2/promise';
+
+export let pool;
+
+// koneksi ke database
+export const init = () => {
+  try {
+    pool = createPool({
+      host: process.env.MYSQL_HOST || 'localhost',
+      user: process.env.MYSQL_USER || 'user',
+      database: process.env.MYSQL_DBNAME || 'devoce',
+      password: process.env.MYSQL_PASSWORD || 'password',
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
+
+    console.debug('Database connected!');
+  } catch (error) {
+    console.error('[Error]: ', error);
+    throw new Error('Fail to connect database!');
+  }
+};
+
+// migrasi database
+export const migration = async () => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    // table companies
+    await pool.query(
+      `
+				CREATE TABLE IF NOT EXISTS companies (
+				id int not null auto_increment,
+				name varchar(255) not null,
+				address varchar(255) not null,
+				email varchar(255) not null,
+				phone varchar(255) not null,
+				primary key (id)
+				)
+      `
+    );
+
+    // table products
+    await pool.query(
+      `
+				CREATE TABLE IF NOT EXISTS products (
+				id int not null auto_increment,
+				name varchar(255) not null,
+				company_id int not null,
+        primary key (id),
+        foreign key (company_id) REFERENCES companies(id) ON DELETE CASCADE
+				)
+      `
+    );
+    // table campuses
+    await pool.query(
+      `
+				CREATE TABLE IF NOT EXISTS campuses (
+				id int not null auto_increment,
+				university_name varchar(255) not null,
+				location varchar(255) not null,
+				website varchar(255) not null,
+        primary key (id)
+				)
+      `
+    );
+
+    // tabel majors
+    await pool.query(
+      `
+				CREATE TABLE IF NOT EXISTS majors (
+				id int not null auto_increment,
+				name varchar(255) not null,
+				campus_id int not null,
+        primary key (id),
+        foreign key (campus_id) REFERENCES campuses(id) ON DELETE CASCADE
+				)
+      `
+    );
+    // tabel talent
+    await pool.query(
+      `
+				CREATE TABLE IF NOT EXISTS talents (
+				id int not null auto_increment,
+				name varchar(255) not null,
+				email varchar(255) not null,
+				skills varchar(255) not null,
+        primary key (id)
+				)
+      `
+    );
+    // tabel community
+    await pool.query(
+      `
+				CREATE TABLE IF NOT EXISTS communities (
+				id int not null auto_increment,
+				name varchar(255) not null,
+				description varchar(255) not null,
+				members varchar(255) not null,
+        primary key (id)
+				)
+      `
+    );
+
+    console.log('Running Migration Successfully!');
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import express, { json } from 'express';
+import cors from 'cors';
+import { router } from './routes.js';
+import { init, migration } from './database/config.js';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3030;
+const host = process.env.HOST || 'localhost';
+
+app.use(cors());
+app.use(json());
+app.use('/api', router);
+
+// 404 endpoint middleware
+app.all('*', (req, res) => {
+  res.status(404).json({ message: `${req.originalUrl} not found!` });
+});
+
+// error handler
+app.use((err, req, res) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message || 'An error occurred.'
+  });
+});
+
+const run = async () => {
+  init();
+  await migration(); // 👈 running migration before server
+  app.listen(port); // running server
+  console.log(`Server run on http://${host}:${port}/`);
+};
+
+run();
