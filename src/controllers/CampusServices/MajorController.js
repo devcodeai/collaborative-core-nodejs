ce4c1aa2ce5bab9f9f26fd63d93bd0d0394853c5
ravaattar <rava.naufal2014@gmail.com>
2023-08-02 13:18:29 +0700

    add: mapping MajorController functionality

---
+++ src/controllers/CampusServices/MajorController.js
@@ -0,0 +1,25 @@
+import { pool } from '../../database/config.js';
+
+const getMajorsByCampusId = async (req, res) => {
+  return res;
+};
+const getMajorsById = async (req, res) => {
+  return res;
+};
+const createMajorByCampusId = async (req, res) => {
+  return res;
+};
+const updateMajorById = async (req, res) => {
+  return res;
+};
+const deleteMajorById = async (req, res) => {
+  return res;
+};
+
+module.exports = {
+  getMajorsByCampusId,
+  getMajorsById,
+  createMajorByCampusId,
+  updateMajorById,
+  deleteMajorById
+};
