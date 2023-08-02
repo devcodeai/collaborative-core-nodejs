29db82eb5433035f1fb77b0b02e3d0cf158e0345
Suryanto Tan <suryanto.tan5@gmail.com>
2023-08-02 15:02:11 +0700

    add company service API

--- src/controllers/CompanyServiceController.js
+++
@@ -1,8 +0,0 @@
-import { pool } from '../database/config.js';
-
-export const getCompanies = async (req, res) => {
-  const [rows] = await pool.query(`
-		SELECT * from companies;
-	`);
-  return res.status(200).json(rows);
-};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,32 @@
 import { Router } from 'express';
-import { getCompanies } from './controllers/CompanyServiceController.js';
+import {
+  createCompany,
+  deleteCompany,
+  editCompany,
+  getCompanies,
+  getCompanyById
+} from './controllers/CompanyServices/CompanyController.js';
+import {
+  createProduct,
+  deleteProduct,
+  editProduct,
+  getProductById,
+  getProducts
+} from './controllers/CompanyServices/ProductController.js';
 
 const router = new Router();
 // Company service
 router.get('/companies', getCompanies);
+router.get('/companies/:id', getCompanyById);
+router.post('/companies/', createCompany);
+router.put('/companies/:id', editCompany);
+router.delete('/companies/:id', deleteCompany);
+
+// Product service
+router.get('/products', getProducts);
+router.get('/products/:id', getProductById);
+router.post('/products/', createProduct);
+router.put('/products/:id', editProduct);
+router.delete('/products/:id', deleteProduct);
 
 export { router };
