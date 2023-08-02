d116666f62181687aa54c0e9c96077ae101e36a4
ravaattar <rava.naufal2014@gmail.com>
2023-08-03 04:45:59 +0700

    update: response on CommunityServices

--- src/controllers/CommunityServices/CommunityController.js
+++ src/controllers/CommunityServices/CommunityController.js
@@ -6,15 +6,23 @@ export const getCommunities = async (_, res) => {
   try {
     const statement = 'SELECT * FROM communities';
     const [rows] = await pool.query(statement);
+    if (rows.length === 0) {
+      res.status(404).json({
+        code: 404,
+        message: 'Communities Not Found'
+      });
+    }
+
     res.status(200).json({
-      status: 200,
-      message: 'Success',
+      code: 200,
+      message: 'Fetch Communities Success',
       data: rows
     });
   } catch (err) {
-    res.status(400).json({
-      status: 400,
-      message: 'Not Found'
+    res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
 };
@@ -26,20 +34,21 @@ export const getCommunityById = async (req, res) => {
     const [rows] = await pool.execute(statement, [id]);
     if (rows.length === 0) {
       return res.status(404).json({
-        status: 404,
+        code: 404,
         message: `Community with ID ${id} Not Found`
       });
     }
 
     res.status(200).json({
-      status: 200,
-      message: 'Success',
+      code: 200,
+      message: `Fetch Community with ID ${id} Success`,
       data: rows[0]
     });
   } catch (err) {
-    res.status(400).json({
-      status: 400,
-      message: err.message
+    res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
 };
@@ -47,6 +56,13 @@ export const getCommunityById = async (req, res) => {
 export const createCommunity = async (req, res) => {
   try {
     const { name, description, members } = req.body;
+    if (!name || !description || !members) {
+      return res.status(400).json({
+        code: 400,
+        message: 'Bad Request'
+      });
+    }
+
     const statement =
       'INSERT INTO communities (name, description, members) VALUES (?, ?, ?)';
     const [result] = await pool.execute(statement, [
@@ -54,9 +70,10 @@ export const createCommunity = async (req, res) => {
       description,
       members
     ]);
-    res.status(201).json({
-      status: 201,
-      message: 'Success',
+
+    res.status(200).json({
+      code: 200,
+      message: 'Create Community Success',
       data: {
         id: result.insertId,
         name,
@@ -65,9 +82,10 @@ export const createCommunity = async (req, res) => {
       }
     });
   } catch (err) {
-    res.status(400).json({
-      status: 400,
-      message: err.message
+    res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
 };
@@ -76,31 +94,43 @@ export const updateCommunityById = async (req, res) => {
   try {
     const id = req.params.id;
     const { name, description, members } = req.body;
-    const statement = 'SELECT * FROM communities WHERE id=?';
-    const [rows] = await pool.execute(statement, [id]);
-    if (rows.length === 0) {
-      return res.status(404).json({
-        status: 404,
-        message: `Community with ID ${id} Not Found`
+    if (!name || !description || !members) {
+      return res.status(400).json({
+        code: 400,
+        message: 'Bad Request'
       });
     }
 
     const updateStatement =
       'UPDATE communities SET name=?, description=?, members=? WHERE id=?';
-    await pool.execute(updateStatement, [name, description, members, id]);
-
-    const selectStatement = 'SELECT * FROM communities WHERE id=?';
-    const [updatedRows] = await pool.execute(selectStatement, [id]);
+    const [updateResult] = await pool.execute(updateStatement, [
+      name,
+      description,
+      members,
+      id
+    ]);
+    if (updateResult.affectedRows === 0) {
+      return res.status(404).json({
+        code: 404,
+        message: `Community with ID ${id} Not Found`
+      });
+    }
 
     res.status(200).json({
-      status: 200,
-      message: 'Success',
-      data: updatedRows[0]
+      code: 200,
+      message: `Update Community with ID ${id} Success`,
+      data: {
+        id: Number(id),
+        name,
+        description,
+        members
+      }
     });
   } catch (err) {
-    res.status(400).json({
-      status: 400,
-      message: err.message
+    res.status(500).json({
+      code: 500,
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
 };
@@ -108,27 +138,24 @@ export const updateCommunityById = async (req, res) => {
 export const deleteCommunityById = async (req, res) => {
   try {
     const id = req.params.id;
-    const statement = 'SELECT * FROM communities WHERE id=?';
-    const [rows] = await pool.execute(statement, [id]);
-    if (rows.length === 0) {
+    const deleteStatement = 'DELETE FROM communities WHERE id=?';
+    const [deleteResult] = await pool.execute(deleteStatement, [id]);
+    if (deleteResult.affectedRows === 0) {
       return res.status(404).json({
-        status: 404,
+        code: 404,
         message: `Community with ID ${id} Not Found`
       });
     }
 
-    const deleteStatement = 'DELETE FROM communities WHERE id=?';
-    await pool.execute(deleteStatement, [id]);
-
     res.status(200).json({
-      status: 200,
-      message: 'Success',
-      data: {}
+      code: 200,
+      message: `Delete Community with ID ${id} Success`
     });
   } catch (err) {
     res.status(500).json({
-      status: 500,
-      message: err.message
+      code: 500,
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
 };
