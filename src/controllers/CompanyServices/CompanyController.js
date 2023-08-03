92c453515985742db4e84c22eb247c00bd3e053c
ravaattar <rava.naufal2014@gmail.com>
2023-08-03 11:57:55 +0700

    update: CompanyServices statsu response

--- src/controllers/CompanyServices/CompanyController.js
+++ src/controllers/CompanyServices/CompanyController.js
@@ -1,109 +1,170 @@
 import { pool } from '../../database/config.js';
 
 export const getCompanies = async (req, res) => {
-  const [rows] = await pool.query('SELECT * from companies;');
+  try {
+    const [rows] = await pool.query('SELECT * from companies;');
+    if (!rows || rows.length == 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: 'Companies Not Found'
+      });
+    }
 
-  if (!rows || rows.length == 0) {
-    return res.status(404).json({
-      message: 'Data Not Found'
+    return res.status(200).json({
+      status: 'Success',
+      message: 'Fetch Companies Success',
+      data: rows
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  return res.status(200).json({
-    message: 'Fetch Company Success',
-    data: rows
-  });
 };
 
 export const getCompanyById = async (req, res) => {
-  const { id } = req.params;
-  const [rows] = await pool.query('SELECT * from companies WHERE id = ?;', id);
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query(
+      'SELECT * from companies WHERE id = ?;',
+      id
+    );
+    if (!rows || rows.length == 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Company with ID ${id} Not Found`
+      });
+    }
 
-  if (!rows || rows.length == 0) {
-    return res.status(404).json({
-      message: 'Data Not Found'
+    return res.status(200).json({
+      status: 'Success',
+      message: `Fetch Company with ID ${id} Success`,
+      data: rows[0]
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-  return res.status(200).json({
-    message: 'Fetch Company Success',
-    data: rows[0]
-  });
 };
 
 export const createCompany = async (req, res) => {
-  const { name, address, email, phone } = req.body;
+  try {
+    const { name, address, email, phone } = req.body;
+    if (!name || !address || !email || !phone) {
+      return res.status(400).json({
+        status: 'Failed',
+        message: 'Bad Request'
+      });
+    }
 
-  if (!name || !address || !email || !phone) {
-    return res.status(400).json({
-      message: 'Bad Request'
+    const [result] = await pool.query(
+      `
+      INSERT into companies (name, address, email, phone)
+      VALUES (?,?,?,?);
+      `,
+      [name, address, email, phone]
+    );
+
+    return res.status(200).json({
+      status: 'Success',
+      message: 'Create Company Success',
+      data: {
+        id: result.insertId,
+        name,
+        address,
+        email,
+        phone
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  await pool.query(
-    `
-    INSERT into companies (name, address, email, phone)
-    VALUES (?,?,?,?);
-    `,
-    [name, address, email, phone]
-  );
-
-  return res.status(200).json({
-    message: 'Create Company Success'
-  });
 };
 
 export const editCompany = async (req, res) => {
-  const { id } = req.params;
-  const { name, address, email, phone } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, address, email, phone } = req.body;
+    if (!name || !address || !email || !phone) {
+      return res.status(400).json({
+        status: 'Failed',
+        message: 'Bad Request'
+      });
+    }
 
-  if (!name || !address || !email || !phone) {
-    return res.status(400).json({
-      message: 'Bad Request'
+    const updated = await pool.query(
+      `
+      UPDATE companies set 
+        name = IFNULL(?, name), 
+        address = IFNULL(?, address),
+        email = IFNULL(?, email),
+        phone = IFNULL(?, phone)
+      WHERE id = ?;
+      `,
+      [name, address, email, phone, id]
+    );
+    if (updated[0].affectedRows === 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Company with ID ${id} Not Found`
+      });
+    }
+
+    return res.status(200).json({
+      status: 'Success',
+      message: `Update Company with ID ${id} Success`,
+      data: {
+        id: Number(id),
+        name,
+        address,
+        email,
+        phone
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  const updated = await pool.query(
-    `
-    UPDATE companies set 
-      name = IFNULL(?, name), 
-      address = IFNULL(?, address),
-      email = IFNULL(?, email),
-      phone = IFNULL(?, phone)
-    WHERE id = ?;
-    `,
-    [name, address, email, phone, id]
-  );
-
-  if (updated[0].affectedRows === 0) {
-    return res.status(404).json({
-      status: 'Not Found',
-      message: `Company with ID ${id} Not Found`
-    });
-  }
-
-  return res.status(200).json({
-    message: 'Edit Company Success'
-  });
 };
 
 export const deleteCompany = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
+    const deleted = await pool.query(
+      `
+      DELETE FROM companies WHERE id = ?;
+      `,
+      id
+    );
+    if (deleted[0].affectedRows === 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Company with ID ${id} Not Found`
+      });
+    }
 
-  const deleted = await pool.query(
-    `
-    DELETE FROM companies WHERE id = ?;
-    `,
-    id
-  );
-
-  if (deleted[0].affectedRows === 0) {
-    return res.status(404).json({
-      status: 'Not Found',
-      message: 'Data Not Found'
+    return res.status(200).json({
+      status: 'Success',
+      message: `Delete Company with ID ${id} Success`,
+      data: {}
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  return res.status(200).json({
-    message: 'Delete Company Success'
-  });
 };
--- src/controllers/CompanyServices/ProductController.js
+++ src/controllers/CompanyServices/ProductController.js
@@ -1,38 +1,56 @@
 import { pool } from '../../database/config.js';
 
 export const getProducts = async (req, res) => {
-  const { company_id } = req.query;
-  const [rows] = await pool.query(
-    'SELECT * from products WHERE company_id = ?;',
-    company_id
-  );
+  try {
+    const { company_id } = req.query;
+    const [rows] = await pool.query(
+      'SELECT * from products WHERE company_id = ?;',
+      company_id
+    );
+    if (!rows || rows.length == 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Products with Company ID ${company_id} Not Found`
+      });
+    }
 
-  if (!rows || rows.length == 0) {
-    return res.status(404).json({
-      message: 'Data Not Found'
+    return res.status(200).json({
+      status: 'Success',
+      message: `Fetch Products with Company ID ${company_id} Success`,
+      data: rows
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  return res.status(200).json({
-    message: 'Fetch Product Success',
-    data: rows
-  });
 };
 
 export const getProductById = async (req, res) => {
-  const { id } = req.params;
-  const [rows] = await pool.query('SELECT * from products WHERE id = ?;', id);
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query('SELECT * from products WHERE id = ?;', id);
+    if (!rows || rows.length == 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Product with ID ${id} Not Found`
+      });
+    }
 
-  if (!rows || rows.length == 0) {
-    return res.status(404).json({
-      message: 'Data Not Found'
+    return res.status(200).json({
+      status: 'Success',
+      message: `Fetch Product with ID ${id} Success`,
+      data: rows[0]
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  return res.status(200).json({
-    message: 'Fetch Product Success',
-    data: rows[0]
-  });
 };
 
 export const createProduct = async (req, res) => {
@@ -40,11 +58,23 @@ export const createProduct = async (req, res) => {
     const { name, company_id } = req.body;
     if (!name || !company_id) {
       return res.status(400).json({
+        status: 'Failed',
         message: 'Bad Request'
       });
     }
 
-    await pool.query(
+    const findCompanyStatement = 'SELECT * FROM companies WHERE id=?';
+    const [companyRows] = await pool.execute(findCompanyStatement, [
+      company_id
+    ]);
+    if (companyRows.length === 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Create Product Failed, Company with ID ${company_id} Not Found`
+      });
+    }
+
+    const [result] = await pool.query(
       `
 			INSERT into products (name, company_id)
 			VALUES (?,?);
@@ -53,18 +83,19 @@ export const createProduct = async (req, res) => {
     );
 
     return res.status(200).json({
-      message: 'Create Product Success'
+      status: 'Success',
+      message: 'Create Product Success',
+      data: {
+        id: result.insertId,
+        name,
+        company_id
+      }
     });
-  } catch (error) {
-    // Foreign key error
-    if (error.errno && error.errno === 1452) {
-      return res.status(404).json({
-        message: 'Data Not Found'
-      });
-    }
-
-    return res.status(500).json({
-      message: 'Internal Server Error'
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
 };
@@ -73,10 +104,21 @@ export const editProduct = async (req, res) => {
   try {
     const { name, company_id } = req.body;
     const { id } = req.params;
-
     if (!name || !company_id) {
       return res.status(400).json({
+        status: 'Failed',
         message: 'Bad Request'
+      });
+    }
+
+    const findCompanyStatement = 'SELECT * FROM companies WHERE id=?';
+    const [companyRows] = await pool.execute(findCompanyStatement, [
+      company_id
+    ]);
+    if (companyRows.length === 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Update Product Failed, Company with ID ${company_id} Not Found`
       });
     }
 
@@ -89,7 +131,6 @@ export const editProduct = async (req, res) => {
     `,
       [name, company_id, id]
     );
-
     if (updated[0].affectedRows === 0) {
       return res.status(404).json({
         status: 'Not Found',
@@ -98,10 +139,15 @@ export const editProduct = async (req, res) => {
     }
 
     return res.status(200).json({
-      message: 'Edit Product Success'
+      status: 'Success',
+      message: `Update Product with ID ${id} Success`,
+      data: {
+        id: Number(id),
+        name,
+        company_id
+      }
     });
   } catch (error) {
-    // Foreign key error
     if (error.errno && error.errno === 1452) {
       return res.status(404).json({
         message: 'Data Not Found'
@@ -115,23 +161,31 @@ export const editProduct = async (req, res) => {
 };
 
 export const deleteProduct = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
+    const deleted = await pool.query(
+      `
+      DELETE FROM products WHERE id = ?;
+      `,
+      id
+    );
+    if (deleted[0].affectedRows === 0) {
+      return res.status(404).json({
+        status: 'Not Found',
+        message: `Product with ID ${id} Not Found`
+      });
+    }
 
-  const deleted = await pool.query(
-    `
-    DELETE FROM products WHERE id = ?;
-    `,
-    id
-  );
-
-  if (deleted[0].affectedRows === 0) {
-    return res.status(404).json({
-      status: 'Not Found',
-      message: 'Data Not Found'
+    return res.status(200).json({
+      status: 'Success',
+      message: `Delete Product with ID ${id} Success`,
+      data: {}
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+      error: err.message
     });
   }
-
-  return res.status(200).json({
-    message: 'Delete Product Success'
-  });
 };
